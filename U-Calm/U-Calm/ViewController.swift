bf9e22cdf24babf29465bc92c98b8b78b55da12a
Khoirunnisa' Rizky Noor Fatimah <49296015+khoirunnisarizky@users.noreply.github.com>
2019-05-20 15:26:02 +0700

    Add files via upload

--- U-Calm/U-Calm/ViewController.swift
+++ U-Calm/U-Calm/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 import AVFoundation
 import AudioToolbox
 
-class ViewController: UIViewController, AVAudioPlayerDelegate {
+class ViewController: UIViewController, AVAudioPlayerDelegate, UITextFieldDelegate {
 
     var audioPlayer : AVAudioPlayer!
     var countNumber : Int = 0
     var totalNumber : Int = 0
+    var bipTextField = UITextField()
+    var bipEvery : String = ""
     
     @IBOutlet weak var butir1: UIImageView!
     @IBOutlet weak var butir2: UIImageView!
@@ -37,6 +39,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     @IBOutlet weak var butir19: UIImageView!
     @IBOutlet weak var heartAppeared: UIImageView!
     
+    @IBOutlet weak var countBip: UILabel!
     @IBOutlet weak var countLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var soundLabel: UIButton!
@@ -51,6 +54,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         }
     }
     
+    
     @IBAction func repeatAction(_ sender: Any) {
         countNumber = 0
         totalNumber = 0
@@ -58,13 +62,39 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         totalLabel.text = "\(totalNumber)"
     }
     
-    @IBOutlet weak var bipEveryOutlet: UITextField!
+    @IBAction func bipEveryButton(_ sender: Any) {
+        showAlert()
+    }
+    
+    func showAlert() {
+        let alert = UIAlertController(title: "", message: "", preferredStyle: .alert)
+        alert.addTextField { (textField) -> Void in
+            self.bipTextField = textField
+            self.bipTextField.delegate = self //REQUIRED
+            self.bipTextField.placeholder = "Bip every"
+        }
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
+            alert.dismiss(animated: true, completion: nil)
+            self.bipEvery = self.bipTextField.text!
+            self.countBip.text = self.bipTextField.text
+        }))
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(fullAnimation(_:)))
         tapGesture.delegate = self as? UIGestureRecognizerDelegate
         view.addGestureRecognizer(tapGesture)
+        heartAppeared.isHidden = true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showAlert()
     }
     
     func playSound(selectedSoundFileName : String) {
@@ -80,8 +110,8 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     }
     
     func setupCount() {
-        let boundNumber = Int(bipEveryOutlet.text!)
-        if countNumber < boundNumber! {
+        let boundNumber = Int(bipEvery)!
+        if countNumber < boundNumber {
             countNumber += 1
         } else {
             countNumber = 1
@@ -99,15 +129,34 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         setupTasbihAnimation(butirChoosen: butir5)
         setupTasbihAnimation(butirChoosen: butir6)
         setupTasbihAnimation(butirChoosen: butir7)
+        setupTasbihAnimation(butirChoosen: butir8)
+        setupTasbihAnimation(butirChoosen: butir9)
+        setupTasbihAnimation(butirChoosen: butir10)
+        setupTasbihAnimation(butirChoosen: butir11)
+        setupTasbihAnimation(butirChoosen: butir12)
+        setupTasbihAnimation(butirChoosen: butir13)
+        setupTasbihAnimation(butirChoosen: butir14)
+        setupTasbihAnimation(butirChoosen: butir15)
+        setupTasbihAnimation(butirChoosen: butir16)
+        setupTasbihAnimation(butirChoosen: butir17)
+        setupTasbihAnimation(butirChoosen: butir18)
+        setupTasbihAnimation(butirChoosen: butir19)
     
         setupCount()
+        heartAppeared.isHidden = true
+        countLabel.isHidden = false
+        countBip.isHidden = false
         
         if soundLabel.currentImage == UIImage(named: "sounds on") {
             playSound(selectedSoundFileName: "tap D-Dzikr")
         }
-        let boundNumberVibrate = countLabel.text
-        if bipEveryOutlet.text == boundNumberVibrate {
+        
+        if countLabel.text == bipEvery {
             AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+            heartAppeared.isHidden = false
+            countLabel.isHidden = true
+            countBip.isHidden = true
+            fadeOut()
         }
         
     }
@@ -116,38 +165,82 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     func setupTasbihAnimation(butirChoosen : UIImageView) {
         let path = UIBezierPath()
         path.move(to: butirChoosen.center)
-        //x += 25 dan y += 26 dari objek target pada main.storyboard
+        //x += 21 dan y += 18 dari objek target pada main.storyboard
         if butirChoosen == butir1 {
-            let targetPoint = CGPoint(x: 299, y: 335)
-            let controlPoint = CGPoint(x: butir1.frame.midX, y: 298)
+            let targetPoint = CGPoint(x: 294, y: 329)
+            let controlPoint = CGPoint(x: view.frame.width/2, y: 280)
             path.addQuadCurve(to: targetPoint, controlPoint: controlPoint)
         }else if butirChoosen == butir2 {
-            let targetPoint = CGPoint(x: 126, y: 335)
+            let targetPoint = CGPoint(x: 121, y: 329)
             let controlPoint = CGPoint(x: butir2.frame.midX, y: butir2.frame.midY)
             path.addQuadCurve(to: targetPoint, controlPoint: controlPoint)
         } else if butirChoosen == butir3 {
-            let targetPoint = CGPoint(x: 95, y: 361)
-            let controlPoint = CGPoint(x: view.frame.width/2, y: 537)
+            let targetPoint = CGPoint(x: 90, y: 355)
+            let controlPoint = CGPoint(x: butir3.frame.midX, y: butir3.frame.midY)
             path.addQuadCurve(to: targetPoint, controlPoint: controlPoint)
         } else if butirChoosen == butir4 {
-            let targetPoint = CGPoint(x: 76, y: 397)
+            let targetPoint = CGPoint(x: 71, y: 391)
             let controlPoint = CGPoint(x: butir4.frame.midX, y: butir4.frame.midY)
             path.addQuadCurve(to: targetPoint, controlPoint: controlPoint)
         } else if butirChoosen == butir5 {
-            let targetPoint = CGPoint(x: 65, y: 437)
+            let targetPoint = CGPoint(x: 60, y: 431)
             let controlPoint = CGPoint(x: butir5.frame.midX, y: butir5.frame.midY)
             path.addQuadCurve(to: targetPoint, controlPoint: controlPoint)
         } else if butirChoosen == butir6 {
-            let targetPoint = CGPoint(x: 66, y: 519)
+            let targetPoint = CGPoint(x: 61, y: 473)
             let controlPoint = CGPoint(x: butir6.frame.midX, y: butir6.frame.midY)
             path.addQuadCurve(to: targetPoint, controlPoint: controlPoint)
         } else if butirChoosen == butir7 {
-            let targetPoint = CGPoint(x: 100, y: 554)
+            let targetPoint = CGPoint(x: 73, y: 513)
             let controlPoint = CGPoint(x: butir7.frame.midX, y: butir7.frame.midY)
             path.addQuadCurve(to: targetPoint, controlPoint: controlPoint)
         } else if butirChoosen == butir8 {
-            let targetPoint = CGPoint(x: 100, y: 554)
-            let controlPoint = CGPoint(x: butir7.frame.midX, y: butir7.frame.midY)
+            let targetPoint = CGPoint(x: 95, y: 548)
+            let controlPoint = CGPoint(x: butir8.frame.midX, y: butir8.frame.midY)
+            path.addQuadCurve(to: targetPoint, controlPoint: controlPoint)
+        } else if butirChoosen == butir9 {
+            let targetPoint = CGPoint(x: 127, y: 575)
+            let controlPoint = CGPoint(x: butir9.frame.midX, y: butir9.frame.midY)
+            path.addQuadCurve(to: targetPoint, controlPoint: controlPoint)
+        } else if butirChoosen == butir10 {
+            let targetPoint = CGPoint(x: 166, y: 590)
+            let controlPoint = CGPoint(x: butir10.frame.midX, y: butir10.frame.midY)
+            path.addQuadCurve(to: targetPoint, controlPoint: controlPoint)
+        } else if butirChoosen == butir11 {
+            let targetPoint = CGPoint(x: 207, y: 596)
+            let controlPoint = CGPoint(x: butir11.frame.midX, y: butir11.frame.midY)
+            path.addQuadCurve(to: targetPoint, controlPoint: controlPoint)
+        } else if butirChoosen == butir12 {
+            let targetPoint = CGPoint(x: 248, y: 590)
+            let controlPoint = CGPoint(x: butir12.frame.midX, y: butir12.frame.midY)
+            path.addQuadCurve(to: targetPoint, controlPoint: controlPoint)
+        } else if butirChoosen == butir13 {
+            let targetPoint = CGPoint(x: 286, y: 575)
+            let controlPoint = CGPoint(x: butir13.frame.midX, y: butir13.frame.midY)
+            path.addQuadCurve(to: targetPoint, controlPoint: controlPoint)
+        } else if butirChoosen == butir14 {
+            let targetPoint = CGPoint(x: 318, y: 548)
+            let controlPoint = CGPoint(x: butir14.frame.midX, y: butir14.frame.midY)
+            path.addQuadCurve(to: targetPoint, controlPoint: controlPoint)
+        } else if butirChoosen == butir15 {
+            let targetPoint = CGPoint(x: 341, y: 513)
+            let controlPoint = CGPoint(x: butir15.frame.midX, y: butir15.frame.midY)
+            path.addQuadCurve(to: targetPoint, controlPoint: controlPoint)
+        } else if butirChoosen == butir16 {
+            let targetPoint = CGPoint(x: 354, y: 473)
+            let controlPoint = CGPoint(x: butir16.frame.midX, y: butir16.frame.midY)
+            path.addQuadCurve(to: targetPoint, controlPoint: controlPoint)
+        } else if butirChoosen == butir17 {
+            let targetPoint = CGPoint(x: 356, y: 431)
+            let controlPoint = CGPoint(x: butir17.frame.midX, y: butir17.frame.midY)
+            path.addQuadCurve(to: targetPoint, controlPoint: controlPoint)
+        } else if butirChoosen == butir18 {
+            let targetPoint = CGPoint(x: 345, y: 391)
+            let controlPoint = CGPoint(x: butir18.frame.midX, y: butir18.frame.midY)
+            path.addQuadCurve(to: targetPoint, controlPoint: controlPoint)
+        } else if butirChoosen == butir19 {
+            let targetPoint = CGPoint(x: 326, y: 355)
+            let controlPoint = CGPoint(x: butir19.frame.midX, y: butir19.frame.midY)
             path.addQuadCurve(to: targetPoint, controlPoint: controlPoint)
         }
         
@@ -176,7 +269,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
             // balikin ke size semula
             self.heartAppeared.transform = CGAffineTransform(scaleX: 1, y: 1)
         }) { (isFinished) in
-//            self.fadeOut()
+            self.fadeOut()
         }
     
     
